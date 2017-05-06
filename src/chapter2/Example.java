b0ce6fca38ef78901f3cc4c4a21363ccbda985ec
NanCarp <nancarpli@163.com>
2017-05-06 10:08:06 +0800

    Initial commit

---
+++ src/chapter2/Example.java
@@ -0,0 +1,55 @@
+package chapter2;
+
+import algs4.In;
+import algs4.StdOut;
+
+import java.util.Comparator;
+
+
+/**
+ * Created by NanCarp on 2017/5/6.
+ */
+public class Example {
+    public static void sort(Comparator[] comparators) {
+
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    // 在单行中打印数组
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    // 测试数组元素是否有序
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;
+    }
+
+    // 从标准输入读取字符串，将它们排序并输出
+    public static void main(String[] args) {
+        String[] a = In.readStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
+
+
+
+
+
+

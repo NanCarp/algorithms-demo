523f53d42c825e013c6f262904f07cf3050eae72
nanliyu <nancarpli@163.com>
2017-06-26 23:49:17 +0800

    shell

---
+++ src/chapter2/shell.java
@@ -0,0 +1,57 @@
+package chapter2;
+
+import algs4.In;
+import algs4.StdOut;
+
+/**
+ * Created by nanca on 6/26/2017.
+ */
+public class shell {
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        int h = 1;
+        while (h < N/3) {
+            h = 3*h + 1;
+        }
+        while (h >= 1) {
+            for (int i = h; i < N; i++) {
+                for (int j = i; j >= h && less(a[j], a[j-h]); j -= h)
+                    exch(a, j, j-h);
+            }
+            h = h/3;
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    // 在单行中打印数组
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    // 测试数组元素是否有序
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;
+    }
+
+    // 从标准输入读取字符串，将它们排序并输出
+    public static void main(String[] args) {
+        String[] a = In.readStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}

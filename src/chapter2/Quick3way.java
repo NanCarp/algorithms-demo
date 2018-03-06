d4ee1f00b8055afed00c74c001c190f40f1463f9
nanliyu <nancarpli@163.com>
2018-03-06 23:40:59 +0800

    1

---
+++ src/chapter2/Quick3way.java
@@ -0,0 +1,29 @@
+package chapter2;
+
+/**
+ * Created by nanca on 3/6/2018.
+ */
+public class Quick3way {
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int lt = lo, i = lo + 1, gt = hi;
+        Comparable v = a[lo];
+        while (i <= gt) {
+            int cmp = a[i].compareTo(v);
+            if (cmp < 0) exch(a, lt++, i++);
+            else if (cmp > 0) exch(a, i, gt--);
+            else i++;
+        }
+        sort(a, lo, lt - 1);
+        sort(a, gt + 1, hi);
+    }
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+}
